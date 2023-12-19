66096dfca0d301b04ea9cac5c6d37f8bc7494182
Wilfredo <drastic87@gmail.com>
2023-12-18 22:50:40 -0500

    nav menu animation

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,6 @@
 import './home.scss';
 import Navigation from '../Components/Navigation/Navigation';
 import Header from '../Components/Header/Header';
-import { useState } from 'react';
 
 export default function Home() {
     return (
