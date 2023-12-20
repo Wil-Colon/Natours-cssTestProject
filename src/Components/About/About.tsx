5fd1fac17ec00c5a9ebea1c9d60eb5965330be64
Wilfredo <drastic87@gmail.com>
2023-12-19 22:42:18 -0500

    nav item animation, about section started

---
+++ src/Components/About/About.tsx
@@ -0,0 +1,56 @@
+import './about.scss';
+import img1 from '../../assets/images/nat-1-large.jpg';
+import img2 from '../../assets/images/nat-2-large.jpg';
+import img3 from '../../assets/images/nat-3-large.jpg';
+
+export default function About() {
+    return (
+        <div className="section-about">
+            <h2 className="section-about__header">
+                Exciting Tours for Adventurous People
+            </h2>
+
+            <div className="section-about__row">
+                <div className="section-about__row__heading-tertiary">
+                    <h3>You're going to fall in love with nature</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Aperiam, ipsum sapiente aspernatur libero repellat quis
+                        consequatur ducimus quam nisi exercitationem omnis earum
+                        qui.
+                    </p>
+                </div>
+
+                <div className="section-about__row__heading-tertiary">
+                    <h3>Live adventures like you never have before</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Asperiores nulla deserunt voluptatum nam.
+                    </p>
+                </div>
+            </div>
+
+            <div className="section-about__composition">
+                <img
+                    src={img1}
+                    alt="photo1"
+                    className="section-about__composition__photo"
+                />
+                <img
+                    src={img2}
+                    alt="photo2"
+                    className="section-about__composition__photo"
+                />
+                <img
+                    src={img3}
+                    alt="photo3"
+                    className="section-about__composition__photo"
+                />
+            </div>
+
+            <a href="about" className="btn-link">
+                Learn more
+            </a>
+        </div>
+    );
+}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,14 @@
 import './home.scss';
 import Navigation from '../Components/Navigation/Navigation';
 import Header from '../Components/Header/Header';
+import About from '../Components/About/About';
 
 export default function Home() {
     return (
         <>
             <Navigation />
             <Header />
+            <About />
         </>
     );
 }
