ff3b5d76cdfa7cef78ca8b9185b33f80f138e6e7
Wilfredo <drastic87@gmail.com>
2024-01-02 21:27:04 -0500

    about section wip

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -11,7 +11,7 @@ export default function About() {
             </h2>
 
             <div className="section-about__row">
-                <div className="section-about__row__heading-tertiary">
+                <div className="section-about__row-heading-tertiary">
                     <h3>You're going to fall in love with nature</h3>
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -19,38 +19,24 @@ export default function About() {
                         consequatur ducimus quam nisi exercitationem omnis earum
                         qui.
                     </p>
-                </div>
 
-                <div className="section-about__row__heading-tertiary">
                     <h3>Live adventures like you never have before</h3>
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Asperiores nulla deserunt voluptatum nam.
                     </p>
                 </div>
+
+                <div className="section-about__row-photos">
+                    <img src={img1} alt="photo1" className="photo photo--p1" />
+                    <img src={img2} alt="photo2" className="photo photo--p2" />
+                    <img src={img3} alt="photo3" className="photo photo--p3" />
+                </div>
             </div>
 
-            <div className="section-about__composition">
-                <img
-                    src={img1}
-                    alt="photo1"
-                    className="section-about__composition__photo"
-                />
-                <img
-                    src={img2}
-                    alt="photo2"
-                    className="section-about__composition__photo"
-                />
-                <img
-                    src={img3}
-                    alt="photo3"
-                    className="section-about__composition__photo"
-                />
-            </div>
-
-            <a href="about" className="section-about__btn-link">
+            {/* <a href="about" className="section-about__btn-link">
                 Learn more
-            </a>
+            </a> */}
         </div>
     );
 }
